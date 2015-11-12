26a12ad7fa7ec86bc7d69c77a23e2354451dcf10
John Armstrong <j.armstrong484@gmail.com>
2015-11-13 09:56:57 +1300

    Added a very basic function to create a new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    username = params.require(:user).permit(:username)
+
+    @user = User.new(username)
+
+    respond_to do |format|
+      if @user.save
+        #format.html { redirect_to @company, notice: 'Company was successfully created.' }
+        format.json { render :json => @user }
+      else
+        #format.html { render :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 end
